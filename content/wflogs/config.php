7bffa576ae4cc59d06724a85baed742a0f462d4e
Dave Slutzkin <daveslutzkin@fastmail.fm>
2025-10-22 06:31:34 +1100

    Add more to gitignore

--- content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"NFKI jYqo;b}xE]/.Nh.r|]P)33z(JM;TFzl0Z)CKV>;~kL_3Ag#B(;IZZ,k+?~V";s:7:"version";s:5:"1.1.0";s:13:"attackDataKey";i:1123;s:11:"wafDisabled";b:0;}
